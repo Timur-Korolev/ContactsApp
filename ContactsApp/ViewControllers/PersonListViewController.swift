ee99e569b2350507e7822aca8e8bf3de1581952e
Timur Korolev <timurkorolev89@gmail.com>
2019-10-06 14:33:21 +0300

    Second Task Realise

--- ContactsApp/ViewControllers/PersonListViewController.swift
+++ ContactsApp/ViewControllers/PersonListViewController.swift
@@ -10,85 +10,33 @@ import UIKit
 
 class PersonListViewController: UITableViewController {
     
-    let contacts = Person.getPersons()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
+    let persons = Person.getPersons()
 
     // MARK: - Table view data source
 
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return contacts.count
-//    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return contacts.count
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return persons.count
     }
-
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return persons[section].name + " " + persons[section].surname
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PersonList", for: indexPath)
-        let name = contacts[indexPath.row].name
-        let surname = contacts[indexPath.row].surname
-        cell.textLabel?.text = name + " " + surname
-
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "Phone Number: " + persons[indexPath.section].phoneNumber
+        case 1:
+            cell.textLabel?.text = "Email: " + persons[indexPath.section].email
+        default:
+            break
+        }
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    // MARK: - Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let indexPath = tableView.indexPathForSelectedRow {
-            let infoVC = segue.destination as! InfoViewController
-            infoVC.phoneNumber = contacts[indexPath.row].phoneNumber
-            infoVC.email = contacts[indexPath.row].email
-        }
-    }
-    
-
 }
